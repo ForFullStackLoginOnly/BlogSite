9964deaf972639162c8a67a70a89cd6c08e18c11
NorthernLion <joppetaja@gmail.com>
2019-03-24 18:17:11 +0200

    7.4 + DevTools

--- blogfront/src/store.js
+++ blogfront/src/store.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import notificationReducer from './reducers/notificationReducer'
 
@@ -9,7 +10,9 @@ const reducer = combineReducers({
 
 const store = createStore(
   reducer,
-  applyMiddleware(thunk)
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
 )
 
 export default store
